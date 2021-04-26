ca09c7d1090a01bc8efc0bf0aac2d6bda080224e
= <56016006+thebush99@users.noreply.github.com>
2021-04-26 09:30:44 +0700

    project file

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'stock',
+      brand: 'ha quoc viet',
+      selectStock: 0,
+      onSale: false,
+      url:
+        'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
+      items: ['50% cotton', '20% wool', '30% ployester'],
+      details: [
+        {
+          id: 1,
+          color: 'green',
+          img: './assets/images/socks_green.jpg',
+          inState: true,
+          quality: 0,
+        },
+        {
+          id: 2,
+          color: 'blue',
+          img: './assets/images/socks_blue.jpg',
+          inState: false,
+          quality: 50,
+        },
+      ],
+      count: 0,
+      blue: true,
+    };
+  },
+  methods: {
+    addItem() {
+      return (this.count += 1);
+    },
+    updateState(index) {
+      this.selectStock = index;
+      console.log(this.selectStock);
+    },
+  },
+  computed: {
+    title() {
+      return this.product + ' ' + this.brand;
+    },
+    img() {
+      return this.details[this.selectStock].img;
+    },
+    inStock() {
+      return this.details[this.selectStock].quality;
+    },
+    brandProduct() {
+      return this.brand + ' ' + this.product;
+    },
+  },
+});
